ced0c01536eae34f3e4ad80d7b3ecb39e43584a3
joaogsleite <joao.leite@isobar.com>
2020-11-18 09:22:11 +0000

    env and api url cleanup

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,7 +1,3 @@
-export const API_ADDRESS = process.env.REACT_APP_API_URL || (
-  process.env.NODE_ENV === 'production' 
-    ? '/api'
-    : 'http://localhost:3004/'
-)
+export const API_ADDRESS = process.env.REACT_APP_API_URL || 'https://funfunz.herokuapp.com'
 
 export const API_MOCK_ENABLED: boolean = true //process.env.NODE_ENV === 'test'
