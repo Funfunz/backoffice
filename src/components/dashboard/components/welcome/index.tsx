15506052b2cd77b66229a085f05e2855b93e0f56
Ruben Costa <ruben.costa@isobar.com>
2020-12-14 10:55:56 +0000

    store dismiss welcome widget on localstore

--- src/components/dashboard/components/welcome/index.tsx
+++ src/components/dashboard/components/welcome/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, memo } from 'react';
+import React, { useState, FC, memo } from 'react';
 import { NavLink } from 'react-router-dom';
 import Widget from './../widget';
 import style from './style.module.scss';
@@ -7,6 +7,18 @@ import Icon from 'components/icon';
 export interface IWelcomeProps {}
 
 const Welcome: FC<IWelcomeProps> = () => {
+
+  const [isDismissed, setIsDismiss] = useState(localStorage.getItem('dismiss-welcome-widget') == 'true' || false); 
+
+  if (isDismissed) {
+    return null;
+  }
+
+  const handleOnDismissClick = () => {
+    localStorage.setItem('dismiss-welcome-widget', 'true');
+    setIsDismiss(true);
+  }
+
   return (
     <Widget full>
       <div className={style.welcomeContainer}>
@@ -31,10 +43,10 @@ const Welcome: FC<IWelcomeProps> = () => {
         </div>
 
         <div className={style.dismissContainer}>
-          <a className={style.dismiss}> 
+          <button className={style.dismissButton} onClick={handleOnDismissClick}> 
             <Icon name="times" />
             Dismiss
-          </a>
+          </button>
         </div>
         
       </div>
