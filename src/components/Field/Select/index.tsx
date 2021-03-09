f78d3d1fc4b359001f28d54b6e6fc3b67bd2f50e
João Leite <joao.leite@isobar.com>
2021-03-09 11:51:14 +0000

    FUNFUNZ-99: prepared fields components for readOnly

--- src/components/Field/Select/index.tsx
+++ src/components/Field/Select/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from "react"
-import { FC, memo } from "react"
-import { IFieldProps } from "components/Field"
+import React, { useCallback, FC, memo } from 'react'
+import classNames from 'classnames'
+
+import { IFieldProps } from 'components/Field'
 import FieldWrapper from 'components/Field/Wrapper'
+
 import classes from './style.module.scss'
 
 export interface ISelectFieldOption {
@@ -20,6 +22,7 @@ const SelectField: FC<ISelectField> = ({
   value,
   onChange,
   options = [],
+  readOnly,
 }) => {
 
   const handleChange = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -31,7 +34,11 @@ const SelectField: FC<ISelectField> = ({
   return (
     <FieldWrapper name={name} label={label}>
       <select
-        className={classes.select}
+        disabled={readOnly}
+        className={classNames({
+          [classes.select]: true,
+          [classes.readOnly]: readOnly,
+        })}
         id={`field-${name}`}
         name={name}
         value={value}
